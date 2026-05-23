8d386b0e07c3dff2bb1a0c47f9e3e35a2679cf83
Laurentius Maximus <22939657+GeneralSergal@users.noreply.github.com>
2026-05-23 20:28:30 +0200

    Update test_golden_run.py

--- tests/test_golden_run.py
+++ tests/test_golden_run.py
@@ -1,8 +1,3 @@
-from src.orp_v2_7.runtime import ORPRuntime
-from src.orp_v2_7.config import CM_V2_7_BASE
+from orp_v2_7.runtime import ORPRuntime
+from orp_v2_7.config import CM_V2_7_BASE
 from cts.cts_harness import CTS2_7
-
-
-def test_e01():
-    system = ORPRuntime(CM_V2_7_BASE)
-    assert CTS2_7(system).run()
