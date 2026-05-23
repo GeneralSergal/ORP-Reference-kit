b3b9b6ed027d3f1b048c7d6ffdd22b520a8f15dd
Laurentius Maximus <22939657+GeneralSergal@users.noreply.github.com>
2026-05-23 19:35:57 +0200

    Update protected_cm.py

---
+++ src/orp_v2_7/protected_cm.py
@@ -0,0 +1,26 @@
+class ProtectedCM:
+    def __init__(self, cm):
+        self._cm = cm
+        self._history = [cm]
+
+    @property
+    def params(self):
+        return self._cm.params
+
+    @property
+    def version(self):
+        return self._cm.version
+
+    def commit(self, new_cm):
+        self._history.append(new_cm)
+        self._cm = new_cm
+
+    def rollback(self):
+        if len(self._history) > 1:
+            self._history.pop()
+            self._cm = self._history[-1]
+
+    def __setattr__(self, key, value):
+        if key in {"params", "_cm"} and hasattr(self, "_cm"):
+            raise AttributeError("I1 VIOLATION: CM is immutable outside GCP")
+        super().__setattr__(key, value)
