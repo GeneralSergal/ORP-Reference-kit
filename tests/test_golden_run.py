a0e19a5d2fad044ed67a4b7693dfca608d59b450
Laurentius Maximus <22939657+GeneralSergal@users.noreply.github.com>
2026-05-23 21:40:40 +0200

    Update test_golden_run.py

--- tests/test_golden_run.py
+++ tests/test_golden_run.py
@@ -1,8 +1,9 @@
 from orp_v2_7.runtime import ORPRuntime
-from orp_v2_7.config import CM_V2_7_BASE
+from orp_v2_7.core_types import ConstraintMatrix
 from cts.cts_harness import CTS2_7
 
 
 def test_e01():
-    system = ORPRuntime(CM_V2_7_BASE)
+    cm = ConstraintMatrix(version="CM-2.7.0")
+    system = ORPRuntime(cm)
     assert CTS2_7(system).run()
