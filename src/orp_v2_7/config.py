ab79c124da475aa9622e789ceef687442279fdfa
Laurentius Maximus <22939657+GeneralSergal@users.noreply.github.com>
2026-05-23 20:46:43 +0200

    Update config.py

--- src/orp_v2_7/config.py
+++ src/orp_v2_7/config.py
@@ -4,6 +4,6 @@ CM_V2_7_BASE = ConstraintMatrix(
     version="CM-2.7.0",
     params={
         "drift_threshold": (0.3, 0.7),
-        "hysteresis": (0.1, 0.3),
+        "hysteresis": (1, 3),
     },
 )
